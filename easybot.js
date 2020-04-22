57498db33bb798266fde39ac9fd4538a47018f18
arieljaimez <arieljaimez@gmail.com>
2020-04-22 15:57:33 -0300

    cambio2

--- easybot.js
+++ easybot.js
@@ -23,16 +23,56 @@ const runbot = async () => {
   console.log("subscribed");
 
   // connect the processMessages callback
-  const msgloop = await driver.reactToMessages(processMessages);
-  console.log("connected and waiting for messages");
+  //   const msgloop = await driver.reactToMessages(processMessages);
+  //   console.log("connected and waiting for messages");
 
   // when a message is created in one of the ROOMS, we
   // receive it in the processMesssages callback
 
   // greets from the first room in ROOMS
-  const sent = await driver.sendToRoom(BOTNAME + " is listening ...", ROOMS[0]);
-  console.log("Greeting message sent");
+  //   const sent = await driver.sendToRoom(BOTNAME + " is listening ...", ROOMS[0]);
+  //   console.log("Greeting message sent");
+
+  await driver.respondToMessages(
+    (err, msg, msgOpts) => {
+      if (err) throw err;
+      console.log("[respond]", JSON.stringify(msg), JSON.stringify(msgOpts));
+      demo(msg).catch(e => console.error(e));
+    },
+    {
+      rooms: ["general"],
+      allPublic: false,
+      dm: true,
+      edited: true,
+      livechat: false
+    }
+  );
 };
+
+// Demo bot-style interactions
+// A: Listen for "tell everyone <something>" and send that something to everyone
+// B: Listen for "who's online" and tell that person who's online
+async function demo (message: IMessage) {
+    console.log(message)
+    if (!message.msg) return
+    if (/tell everyone/i.test(message.msg)) {
+      const match = message.msg.match(/tell everyone (.*)/i)
+      if (!match || !match[1]) return
+      const sayWhat = `@${message.u!.username} says "${match[1]}"`
+      const usernames = await api.users.allNames()
+      for (let username of usernames) {
+        if (username !== botUser.username) {
+          const toWhere = await driver.getDirectMessageRoomId(username)
+          await driver.sendToRoomId(sayWhat, toWhere) // DM ID hax
+          await delay(200) // delay to prevent rate-limit error
+        }
+      }
+    } else if (/who\'?s online/i.test(message.msg)) {
+      const names = await api.users.onlineNames()
+      const niceNames = names.join(', ').replace(/, ([^,]*)$/, ' and $1')
+      await driver.sendToRoomId(niceNames + ' are online', message.rid!)
+    }
+  }
 
 // callback for incoming messages filter and processing
 const processMessages = async (err, message, messageOptions) => {
